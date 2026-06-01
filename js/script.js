198295d9eba48837f0c634c067ff02808e8cadb6
f3llip33 <freirefellipe11@gmail.com>
2026-05-31 21:01:42 -0300

    feat: adição do botão para mostrar a origem dos personagens

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function mudarErvilha() {
+    document.getElementById('pea').innerText =
+    "Planta criada em 2009 no Jogo: Plants vs Zombies, Sendo a Planta inicial"
+    
+}
+
+function mudarCarnivora() {
+    document.getElementById('cho').innerText =
+    "Planta criada em 2009 no Jogo: Plants vs Zombies, Sendo encontrada nos primeiros níveis"
+}
+
+function mudarSunflower() {
+    document.getElementById('son').innerText =
+    "Planta criada em 2009 no Jogo: Plants vs Zombies, Sendo a segunda planta a ser encontrada"
+}
+
+function mudarCactus() {
+    document.getElementById('cactsus').innerText =
+    "Planta criada em 2009 no Jogo: Plants vs Zombies, Sendo encontrada na névoa"
+}
+
+function mudarSoldado() {
+    document.getElementById('sordadin').innerText =
+    "Zumbi criado para esse jogo, servindo como classico soldado em hero shotter"
+}
+
+function mudarEngenheiro() {
+    document.getElementById('engei').innerText =
+    "Zumbi inspirado no mineiro zumbi do Plants vs Zombies, que aparece na fase da névoa"
+}
+
+function mudarCientista() {
+    document.getElementById('sci').innerText =
+    "Zumbi original desse jogo, provavelmente inspirado no médico de team fortress 2"
+}
+
+function mudarAllstar() {
+    document.getElementById('sherek').innerText =
+    "Zumbi criado em 2009 no jogo: Plants vs Zombies, sendo encontrado no gramado de noite"
+}
+
+
