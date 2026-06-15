40ce2facb603c93661eafecb27f3829def38c9bf
f3llip33 <freirefellipe11@gmail.com>
2026-06-14 22:28:45 -0300

    feat: Nova página sobre o Garden Warfare 2

--- js/script.js
+++ js/script.js
@@ -19,6 +19,21 @@ function mudarCactus() {
     "Planta criada em 2009 no Jogo: Plants vs Zombies, Sendo encontrada na névoa"
 }
 
+function mudarCitron() {
+    document.getElementById('citron').innerText =
+    "Planta criada em 2013 no Jogo: Plants vs Zombies 2, Sendo encontrado no Futuro Distante"
+}
+
+function mudarRosa() {
+    document.getElementById('rose').innerText =
+    "Planta Original do Jogo, ela aparece no jogo: Plants vs Zombies Heroes também."
+}
+
+function mudarMilho() {
+    document.getElementById('corn').innerText =
+    "Planta criada em 2009 no Jogo: Plants vs Zombies, Sendo encontrada no telhado"
+}
+
 function mudarSoldado() {
     document.getElementById('sordadin').innerText =
     "Zumbi criado para esse jogo, servindo como classico soldado em hero shotter"
@@ -39,4 +54,18 @@ function mudarAllstar() {
     "Zumbi criado em 2009 no jogo: Plants vs Zombies, sendo encontrado no gramado de noite"
 }
 
+function mudarImp() {
+    document.getElementById('imp').innerText =
+    "Zumbi inspirado no zumbinho do Plants vs Zombies, que aparece junto com o Devorador no Telhado"
+}
+
+function mudarIneedahero() {
+    document.getElementById('super').innerText =
+    "Zumbi original do jogo, que também aparece no jogo: Plants vs Zombies Heroes"
+}
+
+function mudarPirata() {
+    document.getElementById('pirate').innerText =
+    "Zumbi inspirado nos zumbis pirata do jogo: Plants vs Zombies 2"
+}
 
